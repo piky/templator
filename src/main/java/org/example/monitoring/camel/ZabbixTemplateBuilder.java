1b846b3f5ee6fd4415a8da21d90bc74e045ab8e0
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2016-06-09 15:30:36 +0300

    moved data files into new folders

--- src/main/java/org/example/monitoring/camel/ZabbixTemplateBuilder.java
+++ src/main/java/org/example/monitoring/camel/ZabbixTemplateBuilder.java
@@ -8,25 +8,14 @@ public class ZabbixTemplateBuilder extends RouteBuilder {
  
   @Override
   public void configure() throws Exception {
-    from("file:src/data/zabbix?noop=true").to("xslt:templates/zbx_template_add_names.xsl?saxon=true")
+    from("file:src/data/in?noop=true")
+    .log("Loading file: ${in.headers.CamelFileNameOnly}")
+    .to("xslt:templates/to_metrics.xsl?saxon=true")
     .to("file:src/data/result_step1")
-    .to("validator:templates/zbx_template_new.xsd")
+    .to("validator:templates/metrics.xsd")
     .to("log:result?level=DEBUG")
     .to("xslt:templates/to_zabbix_export.xsl?saxon=true")
-    .to("file:src/data/result")
-    .to("validator:templates/zabbix_template.xsd");
-    
-  }
-  
-  /*        <route id="_route1">
-            <from id="_from1" uri="file:src/data/zabbix?noop=true"/>
-            <to id="to_xslt" uri="xslt:templates/zbx_template_add_names.xsl?saxon=true"/>
-            <to id="_to11" uri="file:src/data/result_step1"/>
-            <to id="_to2" uri="validator:templates/zbx_template_new.xsd"/>
-            <to id="_to3" uri="log:result"/>
-            <to id="to_xslt" uri="xslt:templates/to_zabbix_export.xsl?saxon=true"/>
-            <to id="_to4" uri="log:result"/>
-            <to id="_to1" uri="file:src/data/result"/>
-            <to id="_to2" uri="validator:templates/zabbix_template.xsd"/>*/
- 
+    .to("file:src/data/out")
+    .to("validator:templates/zabbix_export.xsd");
+  } 
 }
