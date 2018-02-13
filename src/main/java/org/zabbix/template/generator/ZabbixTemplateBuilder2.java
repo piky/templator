f9c7f8465a3dba25a36084256f06d63f8c920af0
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-02-13 13:17:44 +0300

    drools sample

--- src/main/java/org/zabbix/template/generator/ZabbixTemplateBuilder2.java
+++ src/main/java/org/zabbix/template/generator/ZabbixTemplateBuilder2.java
@@ -1,28 +1,31 @@
 package org.zabbix.template.generator;
 
-import java.io.File;
-import java.util.LinkedHashMap;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.kie.api.event.rule.AgendaEventListener;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.StatelessKieSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.zabbix.template.generator.objects.CPUTemperatureMetric;
 import org.zabbix.template.generator.objects.Metric;
 
+import com.company.license.Applicant;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.JsonPath;
 
 @Component
 public class ZabbixTemplateBuilder2 extends RouteBuilder {
 
-    
+     
+  @Autowired
+  private KieContainer kContainer;	
   @Override
   public void configure() throws Exception {
 	  
-	  
+	
 
 	from("file://src/main/resources/json_test?noop=true&delay=30000&idempotentKey=${file:name}-${file:modified}")
 	    .log("Loading file: ${in.headers.CamelFileNameOnly}")
@@ -43,9 +46,37 @@ public class ZabbixTemplateBuilder2 extends RouteBuilder {
 				
 			}
 		})
+	    //TODO drools. move it
+	    .process(new Processor() {
+			 
+			@Override
+			public void process(Exchange exchange) throws Exception {
+		        /*KieServices ks = KieServices.Factory.get();
+		        KieContainer kContainer = ks.getKieClasspathContainer();*/
+				StatelessKieSession ksession = kContainer.newStatelessKieSession();
+				
+				AgendaEventListener agendaEventListener = new TrackingAgendaEventListener();
+				ksession.addEventListener(agendaEventListener);
+				/*ksession.addEventListener( new DebugAgendaEventListener() );
+		        ksession.addEventListener( new DebugRuleRuntimeEventListener() );*/
+				Applicant applicant = new Applicant( "Mr John Smith", 16 );
+				System.out.println(applicant.isValid());
+//				System.out.println("Rule found:" + ksession.getKieBase().getRule("drl","Is of valid age"));
+				ksession.execute( applicant );
+				//ksession.execute( Arrays.asList(new Object[] {applicant}));
+				System.out.println(applicant.isValid());
+			}
+
+	    
+	    })
 
 	    .marshal().json(JsonLibrary.Jackson,true)
 	    .log("${body}");
+	
+	
+	//TODO 
+	//Here should be validation: check that if type snmp than snmpobject defined, for example
+	//Naming restrictions can also be checked for custom metrics
 
   }
 }
