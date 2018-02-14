61e700a14e6a9866f316fa3d521485efed987c80
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-02-14 13:09:35 +0300

    json out to file

--- src/main/java/org/zabbix/template/generator/ZabbixTemplateBuilder3.java
+++ src/main/java/org/zabbix/template/generator/ZabbixTemplateBuilder3.java
@@ -16,7 +16,9 @@ public class ZabbixTemplateBuilder3 extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		errorHandler(deadLetterChannel("direct:errors"));
-		//catch XSLT terminations
+		
+		//Catch wrong metric prototypes spelling
+		//TODO improve error handling
 		onException(org.zabbix.template.generator.objects.MetricNotFoundException.class)
 		.log(LoggingLevel.ERROR,"${file:name}: Please check metric prototype: ${exception.message}");
 		//other errors
@@ -27,18 +29,10 @@ public class ZabbixTemplateBuilder3 extends RouteBuilder {
 		from("file://src/main/resources/json_test_template?noop=true&delay=30000&idempotentKey=${file:name}-${file:modified}")
 		.log("Loading file: ${in.headers.CamelFileNameOnly}")
 		.unmarshal().json(JsonLibrary.Jackson,Template.class)
-		/*		.process(new Processor() {
-
-			@Override
-			public void process(Exchange exchange) throws Exception {
-				JsonNode json= (JsonNode) exchange.getIn().getBody();
-				ObjectMapper mapper = new ObjectMapper();
-				Template  t = (Template) mapper.convertValue( exchange.getIn().getBody(), Template.class );
-				exchange.getIn().setBody(t);
-			}
-		})*/
 		.marshal().json(JsonLibrary.Jackson,true)
-		.log("${body}");
+		//.log("${body}")
+		.to("file:src/main/resources/json_test_template/out");
+		
 
 
 		//TODO 
