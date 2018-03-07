a88d06dfce74ccb7f8fdf72c0b70212d95baef78
v-zhuravlev <zhuravlev.vitaly@gmail.com>
2018-03-08 00:11:06 +0300

    depends On left for triggers

--- src/main/java/org/zabbix/template/generator/objects/Trigger.java
+++ src/main/java/org/zabbix/template/generator/objects/Trigger.java
@@ -1,10 +1,14 @@
 package org.zabbix.template.generator.objects;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonDeserialize(using = TriggerDeserializer.class)
 public class Trigger {
 
+	private String id;
+	private String documentation;
 	private String prototype;
 	private String name;
 	private String expression;
@@ -14,6 +18,7 @@ public class Trigger {
 	private String url;
 	private ManualClose manualClose = ManualClose.NO;
 	private RecoveryMode recoveryMode;
+	private ArrayList<String> dependsOn = new ArrayList<String>(0);
 	public enum ManualClose implements ZabbixValue {
 
 		YES(1),
@@ -50,6 +55,24 @@ public class Trigger {
 		}
 
 	};	
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getDocumentation() {
+		return documentation;
+	}
+	public void setDocumentation(String documentation) {
+		this.documentation = documentation;
+	}
 	/**
 	 * @return the prototype
 	 */
@@ -121,6 +144,12 @@ public class Trigger {
 	 */
 	public void setRecoveryMode(RecoveryMode recoveryMode) {
 		this.recoveryMode = recoveryMode;
+	}
+	public ArrayList<String> getDependsOn() {
+		return dependsOn;
+	}
+	public void setDependsOn(ArrayList<String> dependsOn) {
+		this.dependsOn = dependsOn;
 	}
 
 }
