97aa0b088f084e79c8d4df0e1912021b153c4266
Maxim Chudinov <maxim.chudinov@zabbix.com>
2020-04-13 12:44:23 +0300

    added ES groups

--- src/main/java/org/zabbix/template/generator/objects/Metric.java
+++ src/main/java/org/zabbix/template/generator/objects/Metric.java
@@ -105,7 +105,8 @@ public abstract class Metric {
 	public enum Group {
 		CPU, Memory, Status, Temperature, Network_interfaces, Internal_items, Zabbix_raw_items, Inventory, Storage,
 		General, Fans, Power_supply, Physical_disks, Virtual_disks, Disk_arrays, Filesystems, Wireless, Nginx, Apache,
-		MySQL, PostgreSQL, RabbitMQ, Redis, Security, Monitoring_agent, Services, HAProxy, Docker, Memcached, Clickhouse
+		MySQL, PostgreSQL, RabbitMQ, Redis, Security, Monitoring_agent, Services, HAProxy, Docker, Memcached, Clickhouse,
+		ES_cluster, ES_nodes
 		// to be extended
 	};
 
