0c6b8646fb3e27914745cee26b45f18f625f88f7
Maxim Chudinov <maxim.chudinov@zabbix.com>
2021-07-07 10:34:14 +0300

    added export of template in YAML format from Zabbix 5.4 and later

--- src/main/java/org/zabbix/template/generator/ZabbixTemplateImporter.java
+++ src/main/java/org/zabbix/template/generator/ZabbixTemplateImporter.java
@@ -2,10 +2,6 @@ package org.zabbix.template.generator;
 
 import org.apache.camel.builder.RouteBuilder;
 
-import org.apache.logging.log4j.Marker;
-import org.apache.logging.log4j.MarkerManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -15,9 +11,6 @@ import org.zabbix.api.ZabbixService;
 @Component
 public class ZabbixTemplateImporter extends RouteBuilder {
 
-	private static final Logger logger = LogManager.getLogger(ZabbixTemplateBuilder.class.getName());
-	private static final Marker TEMPLATE_IMPORTER = MarkerManager.getMarker("TEMPLATE_IMPORTER");
-
 	@Autowired(required = true)
 	private ZabbixService zabbixService;
 
@@ -26,11 +19,23 @@ public class ZabbixTemplateImporter extends RouteBuilder {
 
 		/* ASYNC LOAD TEMPLATES TO ZABBIX(experimental feature) */
 		from("file:{{dir.out}}?noop=true&include={{filter}}&readLock=markerFile&recursive=true&initialDelay={{zabbix.delay}}&delay=1000&idempotentKey=${file:name}-${file:modified}&backoffErrorThreshold=1&backoffMultiplier=60")
+				.routeId("Import XML")
 				.filter().simple("${file:name.ext} == 'xml'").filter()
 				.simple("${file:parent} contains '{{zabbix.template_language}}'").filter()
 				.simple("${file:parent} contains '{{zabbix.version}}'")
 				.log("Loading template into Zabbix: ${in.headers.CamelFileNameOnly}")
-				.bean(zabbixService, "importTemplate");
+				.bean(zabbixService, "importTemplate")
+				.to("direct:export_template_to_yaml");
 
+		from("direct:export_template_to_yaml")
+				.routeId("Export YAML")
+				.setHeader("yamlFilePath", simple("${headers.CamelFileName.replaceFirst('\\.xml','.yaml')}"))
+				.setHeader("zbx_ver",simple("{{zabbix.version}}"))
+				.choice()
+					.when(header("zbx_ver").isEqualTo("5.4"))
+						.log("Unloading template from Zabbix to file: ${headers.yamlFilePath}")
+						.bean(zabbixService, "exportTemplate")
+						.to("file:{{dir.out}}?fileName=${headers.yamlFilePath}")
+				.end();
 	}
 }
